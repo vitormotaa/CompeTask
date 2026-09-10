6173fceb6fadd8df8d7305fae5b503e77ddb26a4
vitormotaa <vitormotaazevedo@gmail.com>
2026-09-10 15:09:03 -0300

    arrumando um erro

--- pp_competask_backend/pp_competask/src/main/java/br/cefetmg/pp_competask/repository/UsuarioRepository.java
+++ pp_competask_backend/pp_competask/src/main/java/br/cefetmg/pp_competask/repository/UsuarioRepository.java
@@ -18,5 +18,5 @@ public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
 
     Usuario findByEmail(String email);
 
-    Usuario findByIdUsuario(String id);
+    Usuario findByIdUsuario(Long id);
 }
